2f8eea815dd94d378f7cec7caa3be5605c81ef03
Gnanasekar03 <138458161+Gnanasekar03@users.noreply.github.com>
2024-09-04 12:10:55 +0530

    added Navbar

---
+++ portfolio_react/src/Components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Hero.css';
+import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+const Hero = () => {
+  return (
+    <div id='Home' className='Hero'>
+        <img src={profile_img} alt="" />
+        <h1><span>Hi, I'm Gnanasekar</span> Front-end Developer..!</h1>
+        <p>I'm a Front-End Developer skilled in HTML, CSS, and JavaScript, with a focus on creating responsive and user-friendly web applications. I love turning ideas into interactive and visually appealing websites.</p>
+        <div className="hero-action">
+            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#Contact'>Connect with me</AnchorLink></div>
+            <div className="hero-resume">My resume</div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
---
+++ portfolio_react/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,72 @@
+import React, { useRef, useState } from "react";
+import "./Navbar.css";
+import underline from "../../assets/nav_underline.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import menu_open from "../../assets/menu_open.svg";
+import menu_close from "../../assets/menu_close.svg";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("Home");
+  const menuref = useRef();
+  const openMenu = () => {
+    menuref.current.style.right = "0";
+  };
+  const closeMenu = () => {
+    menuref.current.style.right = "-350px";
+  };
+
+  return (
+    <div className="navbar">
+      <h1>
+        <span>G</span>nana<span>s</span>ekar
+      </h1>
+      <img src={menu_open} alt="" onClick={openMenu} className="nav-mob-open" />
+      <ul ref={menuref} className="nav-menu">
+        <img
+          src={menu_close}
+          alt=""
+          onClick={closeMenu}
+          className="nav-mob-close"
+        />
+        <li>
+          <AnchorLink className="anchor-link" href="#Home">
+            <p onClick={() => setMenu("Home")}>Home</p>
+          </AnchorLink>
+          {menu == "Home" ? <img src={underline} alt="" /> : <></>}
+        </li>
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#About">
+            <p onClick={() => setMenu("About")}>About Me</p>
+          </AnchorLink>
+          {menu == "About" ? <img src={underline} alt="" /> : <></>}
+        </li>
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#Service">
+            <p onClick={() => setMenu("Service")}>Service</p>
+          </AnchorLink>
+          {menu == "Service" ? <img src={underline} alt="" /> : <></>}
+        </li>
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#Work">
+            <p onClick={() => setMenu("Work")}>Portfolio</p>
+          </AnchorLink>
+          {menu == "Work" ? <img src={underline} alt="" /> : <></>}
+        </li>
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#Contact">
+            <p onClick={() => setMenu("Contact")}>Contact</p>
+          </AnchorLink>
+          {menu == "Contact" ? <img src={underline} alt="" /> : <></>}
+        </li>
+      </ul>
+
+      <div className="nav-connect">
+        <AnchorLink className="anchor-link" offset={50} href="#Contact">
+          Connect With Me
+        </AnchorLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
